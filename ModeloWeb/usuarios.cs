4fb15b32da06ef1da1731858f77af1589e6215cd
Nestor-Carrasco <aqguila00@gmail.com>
2019-12-13 00:06:04 -0300

    first commit

---
+++ ModeloWeb/usuarios.cs
@@ -0,0 +1,19 @@
+namespace ModeloWeb
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class usuarios
+    {
+        [Required]
+        [StringLength(15)]
+        public string nombre_usuario { get; set; }
+
+        [Key]
+        [StringLength(50)]
+        public string clave_usuario { get; set; }
+    }
+}
